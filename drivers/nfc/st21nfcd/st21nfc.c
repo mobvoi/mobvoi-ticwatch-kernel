0fa003708c81a3cd9eb0bc18e9c29ae3de645efd
Jason Fu <zhxfu@mobvoi.com>
2020-04-22 18:28:50 +0800

    drivers: clean st21nfcd debug print
    Change-Id: I8ec4f656f55d1f7ce212c98d75aa4a90c622519f

--- drivers/nfc/st21nfcd/st21nfc.c
+++ drivers/nfc/st21nfcd/st21nfc.c
@@ -15,7 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
-#define DEBUG
+
+/* #define DEBUG */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
