cd933d60aecfcf559dbb4c1c3662d60a2294cb0b
wen.yue <wen.yue@mobvoi.com>
2022-06-10 18:45:33 +0800

    drum:touch:modify self rawdata test parameter
    Change-Id: I4f13f7922d8d0194b0fb0d8637c66d9b9300c0e8

--- drivers/input/touchscreen/zinitix/zinitix_touch.h
+++ drivers/input/touchscreen/zinitix/zinitix_touch.h
@@ -65,8 +65,8 @@
 #define SEC_DND_N_COUNT           10
 #define SEC_DND_U_COUNT           2
 #define SEC_DND_FREQUENCY         99  /* 200khz */
-#define SEC_PDND_N_COUNT          31
-#define SEC_PDND_U_COUNT          9
+#define SEC_PDND_N_COUNT          27
+#define SEC_PDND_U_COUNT          3
 #define SEC_PDND_FREQUENCY        240
 
 #define SEC_SELF_N_COUNT          11 /* >12 */
