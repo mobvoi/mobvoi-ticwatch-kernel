725bada03b304dba71212eff8af484187b57e7f3
panlong.shi <panlong.shi@mobvoi.com>
2022-08-12 21:26:29 +0800

    Revert "fix a recently observed issue where Slate could crash"
    This reverts commit 4adcf6c91ad5a8792c9785ce5f848c3aea4e620f.
    
    Change-Id: I8aa7d4bf42c386eb9b56cfcec8a37fa25cf3cc86

--- drivers/soc/qcom/glink_pkt.c
+++ drivers/soc/qcom/glink_pkt.c
@@ -20,7 +20,7 @@
 #include <linux/termios.h>
 
 /* Define IPC Logging Macros */
-#define GLINK_PKT_IPC_LOG_PAGE_CNT 5
+#define GLINK_PKT_IPC_LOG_PAGE_CNT 2
 static void *glink_pkt_ilctxt;
 
 #define GLINK_PKT_INFO(x, ...)						     \
